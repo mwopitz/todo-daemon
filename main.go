df572d3bb1354ebf2929af545067f11aa61ea32e
Michael Opitz <2935760+mwopitz@users.noreply.github.com>
2025-06-13 21:53:17 +0200

    Acquire file lock before starting server

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"syscall"
 
+	"github.com/gofrs/flock"
 	"github.com/mwopitz/go-daemon/client"
 	"github.com/mwopitz/go-daemon/server"
 	"github.com/urfave/cli/v3"
@@ -22,24 +23,24 @@ func main() {
 		log.Fatalf("fatal: cannot determine home directory: %v", err)
 	}
 
-	socketPath := filepath.Join(home, ".go-daemon.sock")
+	lockFile := filepath.Join(home, ".go-daemon.lock")
+	sockFile := filepath.Join(home, ".go-daemon.sock")
 
 	cmd := &cli.Command{
 		Name: "go-daemon",
-
 		Commands: []*cli.Command{
 			{
 				Name:  "run",
 				Usage: "Run the go-daemon server",
 				Action: func(ctx context.Context, _ *cli.Command) error {
-					return run(ctx, socketPath)
+					return run(ctx, lockFile, sockFile)
 				},
 			},
 			{
 				Name:  "address",
 				Usage: "Get the address of the go-daemon server",
 				Action: func(ctx context.Context, _ *cli.Command) error {
-					return address(ctx, socketPath)
+					return address(ctx, sockFile)
 				},
 			},
 		},
@@ -50,9 +51,19 @@ func main() {
 	}
 }
 
-func run(ctx context.Context, socket string) error {
-	if err := os.Remove(socket); err != nil && !os.IsNotExist(err) {
-		return fmt.Errorf("cannot remove old socket: %w", err)
+func run(ctx context.Context, lockFile, sockFile string) error {
+	lock := flock.New(lockFile)
+	locked, err := lock.TryLock()
+	if err != nil {
+		return fmt.Errorf("cannot acquire lock: %w", err)
+	}
+	if !locked {
+		return fmt.Errorf("cannot acquire lock file %s; already running?", lockFile)
+	}
+	defer lock.Unlock()
+
+	if err := os.Remove(sockFile); err != nil && !os.IsNotExist(err) {
+		return fmt.Errorf("cannot remove socket file: %w", err)
 	}
 
 	srv := server.New()
@@ -63,7 +74,7 @@ func run(ctx context.Context, socket string) error {
 	sigc := make(chan os.Signal, 1)
 	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
 	go func() {
-		errc <- srv.Serve("unix", socket)
+		errc <- srv.Serve("unix", sockFile)
 		close(errc)
 	}()
 
